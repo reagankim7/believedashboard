def17036ee39400de69e0c8a386ddbf3fec28127
reagankim7 <133752991+reagankim7@users.noreply.github.com>
2026-04-07 12:05:09 +0700

    reverse auth bypass

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
 import { redirect } from 'next/navigation'
+import { createSupabaseServerClient } from '@/lib/supabaseServer'
 
-export default function Home() {
-  redirect('/dashboard')
+export default async function Home() {
+  const supabase = await createSupabaseServerClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  // If logged in → dashboard
+  if (user) {
+    redirect('/dashboard')
+  }
+
+  // If not → login
+  redirect('/login')
 }
-
-// import { redirect } from 'next/navigation'
-// import { createSupabaseServerClient } from '@/lib/supabaseServer'
-
-// export default async function Home() {
-//   const supabase = await createSupabaseServerClient()
-
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser()
-
-//   // If logged in → dashboard
-//   if (user) {
-//     redirect('/dashboard')
-//   }
-
-//   // If not → login
-//   redirect('/login')
-// }
